b33b13f81f538233a2091480585eb64a30d7cfd9
Naz Rejepov <nazar.rejepov@qa.com>
2024-08-16 15:41:17 +0100

    update docker install

--- armDocker.sh
+++ armDocker.sh
@@ -17,5 +17,5 @@ echo 10.0.0.5 registry.ca-labs.com >> /etc/hosts
 echo 10.0.0.6 production.ca-labs.com >> /etc/hosts
 mkdir -p /etc/docker/certs.d/registry.ca-labs.com:5000/
 cp /certs/cert.pem /etc/docker/certs.d/registry.ca-labs.com:5000/ca.crt
-curl -L https://github.com/docker/compose/releases/download/2.29.2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+curl -SL https://github.com/docker/compose/releases/download/v2.29.1/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
